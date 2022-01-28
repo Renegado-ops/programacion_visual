d80bc13356c0951a2719973de7fed1bdfef4aa2c
Renegado-ops <Erickssondiaz789@gmail.com>
2022-01-28 10:00:35 -0400

    apariencia del buscador

---
+++ Modulo2/Use_cases/buscador.py
@@ -0,0 +1,20 @@
+#Buscador
+
+print("-"*20)
+buscador = input("Buscador nombre: ")
+
+
+while buscador =="":
+    buscador = input("Buscador nombre: \n")
+
+print("\n\nCantidad de resultados: ", "3 \n")
+print("-"*15)
+
+print("Nombres ", "Apellidos")
+print("-"*15)
+print("Albert", "Garcia")
+print("Marlin", "Vasquez")
+print("Aracelis", "Severino")
+print("-"*15)
+
+
